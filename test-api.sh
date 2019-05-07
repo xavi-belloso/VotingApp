61890c13b4b5a29b00bcd53be8ea74a57e3c3de5
Xavi Belloso <xbelloso@voxelgroup.net>
2019-05-07 16:02:11 +0200

    Test Bash added pass or no

--- test-api.sh
+++ test-api.sh
@@ -3,7 +3,26 @@ curl http://localhost:5000/api/voting \
 --data '["c#","java"]' \
 --header 'Content-Type: application/json'
 
+echo '\n'
+
 curl http://localhost:5000/api/voting \
 --request 'PUT' \
 --data '"java"' \
 --header 'Content-Type: application/json'
+
+echo '\n'
+
+winner=$(curl http://localhost:5000/api/voting \
+--request 'DELETE' \
+--silent \
+--header 'Content-Type: application/json' | jq -r '.winner')
+
+if [ "$winner" == "java" ]; then
+    echo "PASSED!"
+    exit 0
+else
+    echo "FAILED!"
+    exit 1
+fi
+
+echo "The winner is :" $winner
