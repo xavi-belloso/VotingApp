a360ffc179c1cc66b71bca23c6c734f9e6366768
Xavi Belloso <xbelloso@voxelgroup.net>
2019-05-07 15:52:14 +0200

    Add middleware and websockets

--- VotingApp.Api/Controllers/VotingController.cs
+++ VotingApp.Api/Controllers/VotingController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EasyWebSockets;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -14,47 +15,46 @@ namespace VotingApp.Api.Controllers
     [ApiController]
     public class VotingController : ControllerBase
     {
-        public readonly Voting _voting;
+        private readonly Voting _voting;
         private readonly int _step;
         private readonly ILogger<VotingController> _logger;
+        private readonly IWebSocketPublisher _wsPublisher;
 
-        public VotingController(Voting voting, IConfiguration config, ILogger<VotingController> logger)
+        public VotingController(Voting voting, IConfiguration config, ILogger<VotingController> logger, IWebSocketPublisher wsPublisher)
         {
             _voting = voting;
             _step = config.GetValue<int>("VotingStep", 1);
             _logger = logger;
+            _wsPublisher = wsPublisher;
         }
         // GET api/values
         [HttpGet]
-        public object Get()
-        {
-            _logger.LogWarning("Getting information");
-            return _voting.GetState();
-        }
+        public object Get() => _voting.GetState();
 
         // POST api/values
         [HttpPost]
-        public object Post([FromBody] string[] options)
-        {
-            _logger.LogWarning($"Start Voting {JsonConvert.SerializeObject(_voting.GetState())}");
-            _voting.Start(options);
-            return _voting.GetState();
-        }
+        public async Task<object> Post([FromBody] string[] options) =>
+            await ExecuteCommand(() => _voting.Start(options));
 
         // PUT api/values/5
         [HttpPut]
-        public object Put([FromBody] string value)
-        {
-            _voting.Vote(value, _step);
-            return _voting.GetState();
-        }
+        public async Task<object> Put([FromBody] string value) =>
+            await ExecuteCommand(() => _voting.Vote(value, _step));
 
         // DELETE api/values/5
         [HttpDelete]
-        public object Delete()
+        public async Task<object> Delete() =>
+            await ExecuteCommand(() => _voting.Finish());
+
+        private async Task<object> ExecuteCommand(Action command)
         {
-            _voting.Finish();
-            return _voting.GetState();
+            _logger.LogInformation($"Start command {JsonConvert.SerializeObject(_voting.GetState())}");
+            command();
+            _logger.LogInformation($"End Command {JsonConvert.SerializeObject(_voting.GetState())}");
+            var status = _voting.GetState();
+            await _wsPublisher.SendMessageToAllAsync(status);
+            return status;
         }
+
     }
 }
--- VotingApp.Api/Startup.cs
+++ VotingApp.Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EasyWebSockets;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,11 +29,15 @@ namespace VotingApp.Api
         {
             services.AddMvc();
             services.AddSingleton<Voting>();
+            services.AddEasyWebSockets();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseEasyWebSockets();
             app.UseMvc();
         }
     }
